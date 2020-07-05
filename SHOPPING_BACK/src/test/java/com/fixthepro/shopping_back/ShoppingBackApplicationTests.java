41ba30891cc51bb79ce15f829435a647eacdbe0c
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-07-05 13:20:28 -0400

    Completion of back-end spring boot project set up

---
+++ SHOPPING_BACK/src/test/java/com/fixthepro/shopping_back/ShoppingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixthepro.shopping_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
