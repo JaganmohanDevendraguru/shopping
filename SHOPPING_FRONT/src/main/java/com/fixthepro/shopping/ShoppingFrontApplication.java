b2c8f67bede1a1c9209376d6437485e6e9c9cede
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-07-05 10:47:22 -0400

    push spring boot project setup from local

---
+++ SHOPPING_FRONT/src/main/java/com/fixthepro/shopping/ShoppingFrontApplication.java
@@ -0,0 +1,13 @@
+package com.fixthepro.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingFrontApplication.class, args);
+	}
+
+}
---
+++ SHOPPING_FRONT/src/test/java/com/fixthepro/shopping/ShoppingFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixthepro.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
