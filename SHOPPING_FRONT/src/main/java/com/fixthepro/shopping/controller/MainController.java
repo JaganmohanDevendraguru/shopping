466ec588c67e4c3363fe00caadc4e5e398511d74
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-07-05 21:56:18 -0400

    Completed height 100%,footer bottom, and other interface issues.

--- SHOPPING_FRONT/src/main/java/com/fixthepro/shopping/controller/MainController.java
+++ SHOPPING_FRONT/src/main/java/com/fixthepro/shopping/controller/MainController.java
@@ -10,6 +10,24 @@ public class MainController {
 	@GetMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("userClickHome", true);
+		mv.addObject("title", "Home");
+		return mv;
+	}
+	
+	@GetMapping(value= {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("userClickAbout", true);
+		mv.addObject("title", "About Us");
+		return mv;
+	}
+	
+	@GetMapping(value= {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("userClickContact", true);
+		mv.addObject("title", "Contact Us");
 		return mv;
 	}
 }
---
+++ SHOPPING_FRONT/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,5 @@
+<div class="container">
+	<div class="row">
+		<h3>About Us</h3>
+	</div>
+</div>
